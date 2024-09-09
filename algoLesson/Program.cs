05bd8a632e2eead749cbc56c50112548b29b2781
Kavindu Kaveesha <kavindukaveesha16@gmail.com>
2024-09-09 20:23:37 +0530

    Recursion Factorial ok

--- algoLesson/Program.cs
+++ algoLesson/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using algoLesson.Recursion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /**********************Find Factoril****************/
+
+        Console.Write("Enter a number to find its factorial: ");
+
+        // Get input from the user
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            long result = Factorial.FindFactorial(number);
+            Console.WriteLine($"Factorial of {number} is: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+        /**********************End of Find Factoril****************/
+
+    }
+}
